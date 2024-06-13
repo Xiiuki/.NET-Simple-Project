c574ff30025a6124698a9ee695c6b1894769d4c2
Matthias Mews <73477569+Xiiuki@users.noreply.github.com>
2024-06-13 12:52:42 +0200

    Add api parts

---
+++ WpfApp_presAPI/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using WpfApp_presAPI.Models;
+
+namespace WpfApp_presAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Planete> Planetes { get; set; }
+    }
+}
+
---
+++ WpfApp_presAPI/Models/Planete.cs
@@ -0,0 +1,11 @@
+﻿namespace WpfApp_presAPI.Models
+{
+    public class Planete
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Diameter { get; set; }
+        public double Mass { get; set; }
+        public int OrbitPeriod { get; set; }
+    }
+}
